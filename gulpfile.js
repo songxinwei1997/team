b2f8e76137304ba9e604e6182281ef8d456a6aad
sxw <qgx1014>
2018-07-10 10:59:18 +0800

    gulpfile任务完成

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+var server = require('gulp-webserver');
+var sass = require('gulp-sass');
+gulp.task('server', ['sass'], function () {
+    gulp.src('src')
+    .pipe(server({
+        port: 9090,
+        middleware: function (req, res, next) {
+            var pathname = url.parse(req.url).pathname;
+            if (pathname === '/favicon.ico') {
+                return false;
+            }
+            pathname = pathname === '/' ? '/index.html' : pathname;
+            res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+        }
+    }))
+});
+gulp.task('sass', function () {
+    return gulp.src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/css'))
+})
